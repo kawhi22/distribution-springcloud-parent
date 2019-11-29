8a0bd84f950703b6df82e69a8e6b38be40c346ce
kawhi22 <714113262@qq.com>
2019-11-29 16:51:57 +0800

    Initial commit

---
+++ distribution-springcloud-center/src/main/java/com/zz/CenterMain.java
@@ -0,0 +1,13 @@
+package com.zz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class CenterMain {
+    public static void main(String args[]){
+        SpringApplication.run(CenterMain.class,args);
+    }
+}
---
+++ src/main/java/com/zz/distributionspringcloudparent/DistributionSpringcloudParentApplication.java
@@ -0,0 +1,13 @@
+package com.zz.distributionspringcloudparent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DistributionSpringcloudParentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DistributionSpringcloudParentApplication.class, args);
+    }
+
+}
